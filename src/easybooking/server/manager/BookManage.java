e8558446dc8af6900072c98cc3bdb565ee6a2082
Pascal2807 <megerpascal@gmail.com>
2019-12-18 08:11:26 +0100

    Solved a crazy name bug. If the name is BookManager, there is a error. Using the name BookManage, there is no bug

--- src/easybooking/server/manager/BookManage.java
+++ src/easybooking/server/manager/BookManage.java
@@ -2,7 +2,7 @@ package easybooking.server.manager;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.sql.Timestamp;
+import java.security.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,17 +11,17 @@ import java.util.Map;
 import easybooking.server.data.classes.Flight;
 import easybooking.server.data.classes.Reservation;
 import easybooking.server.data.dto.FlightDTO;
-import easybooking.server.remote.IBookManager;
+import easybooking.server.remote.IBookManage;
 
 // THIS IS THE APPLICATION SERVICE CLASS
-public class BookManager extends UnicastRemoteObject implements IBookManager {
+public class BookManage extends UnicastRemoteObject implements IBookManage {
 	
 	private ArrayList<Flight>cacheFlights = new ArrayList<>();
 	
 	private static final long serialVersionUID = 1L;
 	String serverName;
 	
-	public BookManager(String serverName) throws RemoteException {
+	public BookManage(String serverName) throws RemoteException {
 		this.serverName = serverName;
 	}
 	
--- src/easybooking/server/manager/BookManagerServer.java
+++ src/easybooking/server/manager/BookManagerServer.java
@@ -2,7 +2,7 @@ package easybooking.server.manager;
 
 import java.rmi.Naming;
 
-import easybooking.server.remote.IBookManager;
+import easybooking.server.remote.IBookManage;
 
 public class BookManagerServer {
 
@@ -18,7 +18,7 @@ public class BookManagerServer {
 		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
 
 		try {
-			IBookManager service = new BookManager(args[2]);			
+			IBookManage service = new BookManage(args[2]);
 			Naming.rebind(name, service);
 			System.out.println("* Easybooking Service '" + args[2] + "' active and waiting...");
 		} catch (Exception e) {
