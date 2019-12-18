21eb698150c00b07ce4a9e47d4c49dae9f265fcd
16185 <pascal.meger@nordakademie.de>
2019-12-18 01:22:25 +0100

    Prepare db

---
+++ db/create-productsdb.sql
@@ -0,0 +1,11 @@
+/* DELETE 'productsdb' database*/
+DROP SCHEMA IF EXISTS productsdb;
+/* DELETE USER 'sd' AT LOCAL SERVER*/
+DROP USER IF EXISTS 'sd'@'%';
+
+/* CREATE ''productsdb' DATABASE */
+CREATE SCHEMA productsdb;
+/* CREATE THE USER 'sd' AT LOCAL SERVER WITH PASSWORD 'sd' */
+CREATE USER IF NOT EXISTS 'sd'@'%' IDENTIFIED BY 'sd';
+/* GRANT FULL ACCESS TO THE DATABASE FOR THE USER 'sd' AT LOCAL SERVER*/
+GRANT ALL ON productsdb.* TO 'sd'@'%';
